5b1c233d0a5ed364df6a45152c8d9c85a98abfa3
xnxing <xnxing@moreecare.com>
2020-09-23 17:53:22 +0800

    完善vault文档

--- catalog-service/src/main/java/com/thefirstwind/catalogservice/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/thefirstwind/catalogservice/web/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.thefirstwind.catalogservice.exceptions.ProductNotFoundException;
 import com.thefirstwind.catalogservice.services.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +16,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@RefreshScope
 @Slf4j
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-//    private final ProductService productService;
-
-//    @Autowired
-//    public ProductController(ProductService productService){
-//        this.productService = productService;
-//    }
+    @Value("${product.limit}")
+    private Integer productLimit;
 
     @GetMapping("")
     public List<Product> allProducts(){
@@ -38,5 +37,9 @@ public class ProductController {
                 .orElseThrow(() -> new ProductNotFoundException("Product with code ["+code+"] doesn't exist"));
     }
 
+    @GetMapping("/getProductLimit")
+    public Integer getProductLimit(){
+        return productLimit;
+    }
 
 }
