e5611dff1bff4df4a2b6f0d14cd39db224f6b4f5
xnxing <xnxing@moreecare.com>
2020-10-05 15:58:20 +0800

    添加接口约束

--- spring-cloud-hystrix-intro-activity-api/src/main/java/com/thefirstwind/hystrix/activityApi/service/LoginActivityService.java
+++ spring-cloud-hystrix-intro-activity-api/src/main/java/com/thefirstwind/hystrix/activityApi/service/LoginActivityService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class LoginActivityService {
+public class LoginActivityService implements ILoginActivityService{
 
     @Autowired
     private RestTemplate restTemplate;
--- spring-cloud-hystrix-intro-activity/src/main/java/com/thefirstwind/hystrix/activity/controller/LoginActivityController.java
+++ spring-cloud-hystrix-intro-activity/src/main/java/com/thefirstwind/hystrix/activity/controller/LoginActivityController.java
@@ -2,6 +2,7 @@ package com.thefirstwind.hystrix.activity.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.thefirstwind.hystrix.activityApi.service.ILoginActivityService;
 import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.TimeUnit;
 
 @RestController
-public class LoginActivityController {
+public class LoginActivityController implements ILoginActivityService {
 
     @PostMapping("firstLoginActivity")
-    public String firstLoginActivity(@RequestBody Long userId){
+    public String firstLogin(@RequestBody Long userId){
         System.out.println("firstLoginActivity 初始化活动" + userId);
         return "ok";
     }
 
     @PostMapping("firstLoginActivityTimeout")
-    public String firstLoginActivityTimeout(@RequestBody Long userId)  {
+    public String firstLoginTimeout(@RequestBody Long userId)  {
         try {
             TimeUnit.SECONDS.sleep(RandomUtils.nextInt(5) + 1);
         }catch(Exception e){
@@ -30,7 +31,7 @@ public class LoginActivityController {
     }
 
     @PostMapping("firstLoginActivityError")
-    public String firstLoginActivityError(@RequestBody Long userId)  {
+    public String firstLoginFallback(@RequestBody Long userId)  {
         throw new RuntimeException("firstLoginActivity 初始化活动 failed " + userId);
     }
 
